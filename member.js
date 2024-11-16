6f6d6ed19d5550713cf09c4e5a9c220b4090d81d
SergioAR19 <sergio.s427121@cesurformacion.com>
2024-11-16 21:09:58 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,20 @@
+function skillMember() {
+    this.name = "Member";
+    this.id = "member";
+    this.icon = "icon-member";
+    this.description = "Member";
+    this.start = 0;
+    this.end = 0;
+    this.deadline = 0;
+    this.duration = 0;
+    this.isSkill = true;
+    this.isMember = true;
+    this.isLeader = false;
+    this.isTask = false;
+    this.isSprint = false;
+    this.isStory = false;
+    this.isBacklog = false;
+    this.isProject = false;
+    this.isProduct = false;
+    this.isCompany = false;
+}
